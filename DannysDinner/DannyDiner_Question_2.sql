a2f8d162efd6075bf705d1446dcc77972afbc0cc
Khang Tran <trank9553@gmail.com>
2025-03-21 23:41:20 +0700

    Delete DannysDinner directory

--- DannysDinner/DannyDiner_Question_2.sql
+++
@@ -1,9 +0,0 @@
-USE DannysDinerDBUI;
-GO
--- Question 2: How many days has each customer visited the restaurant?
-SELECT
-	customer_id,
-	COUNT(DISTINCT order_date) AS total_visited_days
-FROM sales
-GROUP BY customer_id
-ORDER BY customer_id ASC;
