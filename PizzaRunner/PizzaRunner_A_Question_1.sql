6017374fb7f031326235702ab3e126c8345a76ba
Khang Tran <trank9553@gmail.com>
2025-04-12 21:55:22 +0700

    Delete PizzaRunner directory

--- PizzaRunner/PizzaRunner_A_Question_1.sql
+++
@@ -1,6 +0,0 @@
-USE PizzaRunnerDBUI;
-GO
--- A. Pizza Metrics
--- Question 1: How many pizzas were ordered?
-SELECT COUNT(order_id) AS total_ordered_pizzas
-FROM customer_orders;
--- PizzaRunner/PizzaRunner_A_Question_2.sql
+++
@@ -1,6 +0,0 @@
-USE PizzaRunnerDBUI;
-GO
--- A. Pizza Metrics
--- Question 2: How many unique customer orders were made?
-SELECT COUNT(DISTINCT order_id) AS total_unique_orders
-FROM customer_orders;
