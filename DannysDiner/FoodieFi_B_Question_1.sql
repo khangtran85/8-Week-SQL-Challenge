40a52f05ea4e2b3244e23a6f3af5a3b0d660d71b
Khang Tran <trank9553@gmail.com>
2025-04-12 21:59:35 +0700

    Delete DannysDiner directory

--- DannysDiner/FoodieFi_B_Question_1.sql
+++
@@ -1,6 +0,0 @@
-USE FoodieFiDBUI;
-GO
--- B. Data Analysis Questions
--- Question 1: How many customers has Foodie-Fi ever had?
-SELECT COUNT(DISTINCT customer_id) AS total_customers
-FROM subscriptions;
